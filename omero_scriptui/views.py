30943a533759404576c409994178a718a05fbbd1
William Moore <w.moore@dundee.ac.uk>
2024-05-09 09:58:58 +0100

    Fix script_run URL and csrf_token

--- omero_scriptui/views.py
+++ omero_scriptui/views.py
@@ -45,5 +45,8 @@ def index(request, conn=None, **kwargs):
 @login_required()
 def import_from_csv(request, conn=None, **kwargs):
 
-    context = {}
+    script_service = conn.getScriptService()
+    sid = script_service.getScriptID("/omero/annotation_scripts/Import_from_csv.py")
+
+    context = {"script_id": sid}
     return render(request, "omero_scriptui/import_from_csv.html", context)
